1f9e1245bc064c9869327c1ba4ed9538945f3e41
Efrain Bautista <efrain@thibau.io>
2026-09-16 07:33:00 -0500

    feat(app): add developer count tracking.

--- src/app.vue
+++ src/app.vue
@@ -2,13 +2,23 @@
 import {ref} from "vue";
 import DeveloperRegistration from "./greetings/presentation/components/developer-registration.vue";
 import DeveloperGreeting from "./greetings/presentation/components/developer-greeting.vue";
+import DeveloperCountShow from "./greetings/presentation/components/developer-count-show.vue";
 
 const registeredDeveloper = ref(null);
 const hasRegistered = ref(false);
+const developerCount = ref(0);
 
 function updateRegisteredDeveloperInfo(payload) {
   registeredDeveloper.value = payload.developer;
   hasRegistered.value = true;
+  updateDeveloperCount(payload.developer);
+
+}
+
+function updateDeveloperCount(developer) {
+  if (developer.isRegisterable()) {
+    developerCount.value++;
+  }
 }
 </script>
 
@@ -18,4 +28,5 @@ function updateRegisteredDeveloperInfo(payload) {
       @developer-registered="updateRegisteredDeveloperInfo" />
 
   <developer-greeting v-if="hasRegistered" :developer="registeredDeveloper"/>
+  <developer-count-show :developer-count="developerCount" />
 </template>
